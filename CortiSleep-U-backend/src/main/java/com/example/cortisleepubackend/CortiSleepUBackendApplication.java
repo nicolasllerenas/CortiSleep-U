ff207be0758f2637e646bb0cfbe3dd84968f8713
nicolasllerenas <nicolas.llerena@utec.edu.pe>
2025-08-26 20:43:59 -0500

    backend framework creado

---
+++ CortiSleep-U-backend/src/main/java/com/example/cortisleepubackend/CortiSleepUBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cortisleepubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CortiSleepUBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CortiSleepUBackendApplication.class, args);
+    }
+
+}
---
+++ CortiSleep-U-backend/src/test/java/com/example/cortisleepubackend/CortiSleepUBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cortisleepubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CortiSleepUBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
